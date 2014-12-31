c1806a77fbfed0cfecc088f8bafe57450cba2c46
John Robinson <soco@uw.edu>
2014-12-31 13:17:06 -0800

    change from fromtimestap to utcfromtimestamp to avoid converting into localtime

--- import.py
+++ import.py
@@ -11,13 +11,17 @@ from pymongo import MongoClient
 
 #
 # convert an RFC822 date to a datetime
-# borrowed from: https://gist.github.com/robertklep/2928188
+# borrowed and modified from: https://gist.github.com/robertklep/2928188
+#
+# original used fromtimestamp instead of utcfromtimestamp, which converts it to local time.
+# currently, all of the tweets seem to just use utc, so we can ignore the timestamp (though
+# it's unclear if mktime_tz actually utilizes the info or just ignores it. )
 #
 def convertRFC822ToDateTime(rfc822string):
 	"""
 		convert an RFC822 date to a datetime
 	"""
-	return datetime.fromtimestamp(email.utils.mktime_tz(email.utils.parsedate_tz(rfc822string)))
+	return datetime.utcfromtimestamp(email.utils.mktime_tz(email.utils.parsedate_tz(rfc822string)))
 
 
 
